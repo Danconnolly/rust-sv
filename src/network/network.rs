db5de89e6563c40bd6e4cefa3a8d4a5febb9b438
Daniel Connolly <daniel@dconnolly.com>
2023-12-27 21:04:39 +0000

    add regtest

--- src/network/network.rs
+++ src/network/network.rs
@@ -10,6 +10,7 @@ pub enum Network {
     Mainnet = 0,
     Testnet = 1,
     STN = 2,
+    Regtest = 3,
 }
 
 impl Network {
@@ -19,6 +20,7 @@ impl Network {
             x if x == Network::Mainnet as u8 => Ok(Network::Mainnet),
             x if x == Network::Testnet as u8 => Ok(Network::Testnet),
             x if x == Network::STN as u8 => Ok(Network::STN),
+            x if x == Network::Regtest as u8 => Ok(Network::Regtest),
             _ => {
                 let msg = format!("Unknown network type: {}", x);
                 Err(Error::BadArgument(msg))
@@ -32,6 +34,7 @@ impl Network {
             Network::Mainnet => 8333,
             Network::Testnet => 18333,
             Network::STN => 9333,
+            Network::Regtest => 18444,
         }
     }
 
@@ -41,6 +44,7 @@ impl Network {
             Network::Mainnet => [0xe3, 0xe1, 0xf3, 0xe8],
             Network::Testnet => [0xf4, 0xe5, 0xf3, 0xf4],
             Network::STN => [0xfb, 0xce, 0xc4, 0xf9],
+            Network::Regtest => [0xda, 0xb5, 0xbf, 0xfa],
         }
     }
 
@@ -117,6 +121,41 @@ impl Network {
                     txns: vec![tx],
                 }
             }
+            Network::Regtest => {
+                let header = BlockHeader {
+                    version: 1,
+                    prev_hash: Hash256([0; 32]),
+                    merkle_root: Hash256::decode(
+                        "0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+                    )
+                        .unwrap(),
+                    timestamp: 1296688602,
+                    bits: 0x207fffff,
+                    nonce: 2,
+                };
+
+                let tx = Tx {
+                    version: 1,
+                    inputs: vec![TxIn {
+                        prev_output: OutPoint {
+                            hash: Hash256([0; 32]),
+                            index: 0xffffffff,
+                        },
+                        unlock_script: Script(hex::decode("04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73").unwrap()),
+                        sequence: 0xffffffff,
+                    }],
+                    outputs: vec![TxOut {
+                        satoshis: 5000000000,
+                        lock_script: Script(hex::decode("4104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac").unwrap()),
+                    }],
+                    lock_time: 0,
+                };
+
+                Block {
+                    header,
+                    txns: vec![tx],
+                }
+            }
         }
     }
 
@@ -131,6 +170,10 @@ impl Network {
                 Hash256::decode("000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943")
                     .unwrap()
             }
+            Network::Regtest => {
+                Hash256::decode("0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206")
+                    .unwrap()
+            }
         }
     }
 
@@ -140,6 +183,7 @@ impl Network {
             Network::Mainnet => 0x00,
             Network::Testnet => 0x6f,
             Network::STN => 0x6f,
+            Network::Regtest => 0x6f,
         }
     }
 
@@ -149,6 +193,7 @@ impl Network {
             Network::Mainnet => 0x05,
             Network::Testnet => 0xc4,
             Network::STN => 0xc4,
+            Network::Regtest => 0xc4,
         }
     }
 
@@ -166,6 +211,7 @@ impl Network {
                 "testnet-seed.bitcoincloud.net".to_string(),
             ],
             Network::STN => vec!["stn-seed.bitcoinsv.io".to_string()],
+            Network::Regtest => vec![],
         }
     }
 
